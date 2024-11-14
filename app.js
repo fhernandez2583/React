7b142d76e460de0d754b1d6a48750fb892486e96
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-14 17:16:12 -0600

    added onclick funfact

--- app.js
+++ app.js
@@ -11,10 +11,19 @@ const background = (
   <img class="background" alt="ocean" src="/images/ocean.jpg" />
 );
 
+function displayFact(e) {
+  const animal = e.target.alt
+  const index = Math.floor(Math.random() * animals[animal].facts.length);
+  const funFact = animals[animal].facts[index];
+  const p = document.getElementById('fact');
+  p.innerHTML = funFact
+}
+
 const images = [];
 for (const animal in animals) {
   const image = (
     <img
+      onClick={displayFact}
       key={animal}
       className="animal"
       alt={animal}
@@ -30,7 +39,8 @@ const animalFacts = (
   <div>
     <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
     {background}
-    <div className="animals">{images}</div>
+    <p id='fact'></p>
+    <div className='animals'>{images}</div>
   </div>
 );
 
